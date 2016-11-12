8a91f4b4f35d75c78c7266d2be57478fc965bb73
Felipe Silva <felipeacelino@hotmail.com>
2016-11-12 00:12:12 -0200

    First commit

---
+++ script.js
@@ -0,0 +1,53 @@
+(function(){
+
+	var heart = document.querySelector('.heart-container');
+	var parts = document.querySelectorAll('.heart');
+	var campo = document.querySelector('#nome');
+	var nome = '';
+
+	var animar = function animar() {
+		nome = campo.value.toString().toLowerCase();
+		switch(nome){
+			case 'beatriz':
+				Array.prototype.forEach.call(parts, function (part) {			
+					part.style.WebkitTransition = 'background 0.5s';
+					part.style.MozTransition = 'background 0.5s';
+					part.style.background = '#FF3F3F';
+				});
+				heart.className = 'heart-container heart-animate-fast';
+				break;
+			case 'palmeiras':
+				Array.prototype.forEach.call(parts, function (part) {				
+					part.style.WebkitTransition = 'background 0.5s';
+					part.style.MozTransition = 'background 0.5s';
+					part.style.background = '#22F340';
+				});
+				heart.className = 'heart-container heart-animate-slow';
+				break;
+			case 'felipe':
+				Array.prototype.forEach.call(parts, function (part) {				
+					part.style.WebkitTransition = 'background 0.5s';
+					part.style.MozTransition = 'background 0.5s';
+					part.style.background = '#656DFF';
+				});
+				heart.className = 'heart-container heart-animate';
+				break;
+			default:
+				Array.prototype.forEach.call(parts, function (part) {				
+					part.style.WebkitTransition = 'background 0.5s';
+					part.style.MozTransition = 'background 0.5s';
+					part.style.background = '#CCC';
+				});
+				heart.className = 'heart-container';
+		}
+	} 
+
+	campo.addEventListener('keyup', animar);
+	campo.addEventListener('paste', animar);
+
+}());
+
+
+
+
+
